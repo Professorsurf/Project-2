83da4815e156605d06a91a934bf359649d5eb947
Professorsurf <guevara.a.gabriel@gmail.com>
2022-02-27 13:18:05 -0800

    working AlphaAdvatage API tested

--- dbTest.js
+++ dbTest.js
@@ -1,19 +1,87 @@
-var request = require('request');
-var requestOptions = {
-        'url': 'https://api.tiingo.com/api/test/',
-        'headers': {
-            'Content-Type': 'application/json',
-            'Authorization': 'Token 0bb310a6a2128b6821ab844dc4a7ed550a5ed935'
-            }
-        };
 
-request(requestOptions,
-        function(error, response, body) {
-            console.log(body);
-        }
-);
+'use strict';
+var request = require('request');
+var url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=';
+
+request.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is successfully parsed as a JSON object:
+      console.log(data);
+    }
+});
+
+
+// var yahooFinance = require('yahoo-finance');
+
+// yahooFinance.historical({
+//   symbol: 'AAPL',
+//   from: '2012-01-01',
+//   to: '2012-12-31',
+//   // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+// }, function (err, quotes) {
+//   //...
+// });
+
+// // This replaces the deprecated snapshot() API
+// yahooFinance.quote({
+//   symbol: 'AAPL',
+//   modules: [ 'price', 'summaryDetail' ] // see the docs for the full list
+// }, function (err, quotes) {
+//   // ...
+// });
+
+
+
+// RAPID API
+
+// var axios = require("axios").default;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
+//   params: {symbol: 'AMRN', region: 'US'},
+//   headers: {
+//     'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
+//     'x-rapidapi-key': 'c598bacb68msh2eac0357e70881ep15fcdcjsn6c2f28ba5520'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+
+
+// Tiingo
+// var request = require('request');
+// var requestOptions = {
+//         'url': 'https://api.tiingo.com/api/test/',
+//         'headers': {
+//             'Content-Type': 'application/json',
+//             'Authorization': 'Token 0bb310a6a2128b6821ab844dc4a7ed550a5ed935'
+//             }
+//         };
+
+// request(requestOptions,
+//         function(error, response, body) {
+//             console.log(body);
+//         }
+// );
 
 // document.addEventListener('DOMContentLoaded', ()=>{
+
+
 
 // // MAKE FETCH HAPPEN  FOR DATA.NASDAQ.COM
 
--- readme.md
+++ readme.md
@@ -1,2 +1,35 @@
 # Project-2
-[add erd](./ERD.drawio.png)
+TIME_SERIES_WEEKLY FINANCIAL MARKETS RESEARCH
+
+
+- This project will allow a user to research the US stock market and save data to their profile
+- The API being used is offered Alpha Vantage
+
+
+User Stories 
+
+- [ ] As a user I would like to enter a company name I want to research
+- [ ] As a user I would like to see the ticker symbol that matches the company I am researching
+- [ ] As a user I would like to see the high and low plus weekly volume for a selected company to include the current weekly and previous weekly close plus the change in percent from previous week 
+- [ ] As a user I would like to tag a specific company or ticker so it saves it to my favorites/research que
+- [ ] As a user I would like to add notes to my selected companies save in the tagged research que
+UPDATE AND DELETE
+
+models
+
+USER ADD STOCKS TO WATCH
+NOTES ADD TO STOCKS FOLLOWED
+
+STRETCH GOAL -
+- [ ] As a user I would like to see a graph chart of the historical data
+STYLING SUCH AS GREEN POSITIVE AND RED NEGATIVE
+STREMING TICKER BANNER
+
+Tech Stack
+Postgres
+Express
+Node.JS
+CSS/Bootstrap
+
+![add erd](./ERD.drawio.png)
+
