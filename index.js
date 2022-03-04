aba1ade073bfd6279070bfd425fef7f093a4edf8
Professorsurf <guevara.a.gabriel@gmail.com>
2022-03-03 22:05:56 -0800

    update faves

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const express = require('express') // import express
-const app = express() // create an express instance
-const ejsLayouts = require('express-ejs-layouts') // import ejs layouts
-require('dotenv').config() // allows us to access env vars
+const express = require('express') 
+const app = express() 
+const ejsLayouts = require('express-ejs-layouts') 
+require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const cryptoJS = require('crypto-js')
-const db = require('./models/index.js')
+const db = require('./models')
+const axios = require('axios')
+const router = require('./controllers/users.js')
+const { all } = require('./controllers/users.js')
 
-// MIDDLEWARE
-app.set('view engine', 'ejs') // set the view engine to ejs
-app.use(ejsLayouts) // tell express we want to use layouts
-app.use(cookieParser()) // gives us access to req.cookies
-app.use(express.urlencoded({extended: false})) // body parser (to make req.body work)
-
-// CUSTOM LOGIN MIDDLEWARE
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use(cookieParser()) 
+app.use(express.urlencoded({extended: false})) 
 app.use(async (req, res, next)=>{
     if(req.cookies.userId){
-        // decrypting the incoming user id from the cookie
         const decryptedId = cryptoJS.AES.decrypt(req.cookies.userId, process.env.SECRET)
-        // converting the decrypted id into a readable string
         const decryptedIdString = decryptedId.toString(cryptoJS.enc.Utf8)
-        // querying the db for the user with that id
         const user = await db.user.findByPk(decryptedIdString)
-        // assigning the found user to res.locals.user in the routes, and user in the ejs
         res.locals.user = user
     } else res.locals.user = null
-    next() // move on to next middleware
+    next() 
 })
 
 // CONTROLLERS
@@ -34,17 +30,12 @@ app.use('/users', require('./controllers/users.js'))
 app.get('/', (req, res)=>{
     res.render('home.ejs')
 })
-
-'use strict';
-const { default: axios } = require('axios');
-const request = require('axios');
-const router = require('./controllers/users');
-const { response } = require('express')
-require('dotenv').config()
-
-// router.get('/users/profile', (req, res) => {
-//     console.log(req.query.stockSearch)
-    let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GOOG&apikey=${process.env.Alphavantage_API_KEY}`;
+app.get('/users/search', (req, res)=>{
+    res.render('users/search.ejs')
+})
+app.get('/users/results', (req, res) => {
+    console.log(req.query.stockSearch)
+    let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.query.stockSearch}&apikey=${process.env.Alphavantage_API_KEY}`;
     const options = {
     headers: {
         'User-Agent': 'request',
@@ -53,10 +44,47 @@ require('dotenv').config()
     } 
     axios.get(url, options)
     .then(response => {
-    // console.log(response.data)
-    console.log("bracket",response.data["Global Quote"])
-    // res.render('users/results.ejs', {stockSearch: response.data["Global Quote"]})
+    console.log(response.data)
+    res.render('users/results.ejs', {stocks: response.data["Global Quote"]})
     })
+})
+
+app.post('/users/results', (req, res) => {
+    db.user.findOne ({
+        where: {id: res.locals.user.id}
+    })
+    .then(founduser => {
+        founduser.createStock({
+            symbol:req.body.symbol, high:req.body.high, low:req.body.low, volume:req.body.volume
+        })
+    })
+    res.redirect('/users/search')
+})
+
+
+
+// FAVES
+// app.get('/users/faves', async (req, res) => {
+//     // res.send('show me some faves')
+//     try {
+//         const allFaves = await db.fave.findAll()
+//         res.json(allFaves)
+//     } catch (err) {
+//         console.log(err)
+//     }
+// })
+
+// app.post('/users/faves', async (req, res) => {
+//     // console.log(req.body)
+//     // db.fave.create
+//     try {
+//         await db.fave.create({
+//             stocks: req.body.allFaves,
+//         })
+//         res.redirect('/faves')
+//     } catch (error) {
+//         console.log(error)
+//     }
 // })
 
 const PORT = process.env.PORT || 3001
